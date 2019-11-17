7a1893d238c057cef33b6916a5493bcd83ea4531
Strangman <55458548+Strangman@users.noreply.github.com>
2019-11-17 18:19:05 +0200

    Add files via upload

---
+++ Homework06/Homework06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var characters = PersonsService.GetPersons();
+            var personal = characters.Select(x => new { PersonDescription = $"{x.FirstName} {x.LastName} {x.Gender} {x.Age}"}) ;
+            foreach (var a in personal)
+                Console.WriteLine(a.PersonDescription);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework06/Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var characters = PersonsService.GetPersons();
+            var personale = characters.Where(x => x.Age > 100).GroupBy(x => x.Gender).GroupBy(x => x.Count()).Select(x=> new {Group =x ,x.Key, Count = x.Count() });
+            foreach (var a in personale)
+            {
+                Console.WriteLine(a.Key);
+                foreach (var b in a.Group)
+                {
+                    Console.WriteLine(b.Key);
+                    foreach (var c in b)
+                        Console.WriteLine(c);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
